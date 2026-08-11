31866f88df6b84870cef39a0839ed175c5f3e39c
GUILHERME MONTEIRO RAMOS <1051392611031@fatec.corp>
2026-08-11 09:28:19 -0300

    Usuario_Start

---
+++ src/main/java/com/migopan/api/model/Usuario.java
@@ -0,0 +1,50 @@
+package com.migopan.api.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Getter @Setter 
+@NoArgsConstructor @AllArgsConstructor
+public class Usuario{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false, name = "email_verificado")
+    private Boolean emailVerificado;
+
+    @Column(nullable = false, length = 255)
+    private String senha_hash;
+
+    @Column(nullable = false)
+    private Integer streak;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal dinheiro = BigDecimal.ZERO;
+
+    @Column(nullable = false, name = "data_criacao", updatable = false)
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+     
+    @Column(nullable = false)
+    private Boolean ativo = true;
+}
