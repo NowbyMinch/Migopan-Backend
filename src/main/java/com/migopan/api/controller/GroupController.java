dd304e50f34741aa289372662d148fc08952c1fb
GUILHERME MONTEIRO RAMOS <1051392611031@fatec.corp>
2026-08-17 08:59:20 -0300

    a

--- src/main/java/com/migopan/api/controller/GroupController.java
+++ src/main/java/com/migopan/api/controller/GroupController.java
@@ -19,9 +19,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
-
 @RestController
 @RequestMapping("/api/groups")
 @CrossOrigin(origins = "*")
---
+++ src/main/java/com/migopan/api/dto/UsuarioResponseDTO.java
@@ -0,0 +1,5 @@
+package com.migopan.api.dto;
+
+import com.migopan.api.model.Usuario;
+
+public record UsuarioResponseDTO( Long id, String nome, String email, )
