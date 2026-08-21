e70a8be8115f2bd8d3cb4f252ccf4c07a1bcaca2
GUILHERME MONTEIRO RAMOS <1051392611031@fatec.corp>
2026-08-21 08:15:24 -0300

    a

--- src/main/java/com/migopan/api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/migopan/api/security/JwtAuthenticationFilter.java
@@ -9,5 +9,39 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    // .... Continuar
+    private final JwtService jwtService;
+    private final UsuarioRepository usuarioRepository;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UsuarioRepository usuarioRepository) {
+        this.jwtService = jwtService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+            String token = null;
+            if (request.getCookies() != null) {
+                for (Cookie cookie : request.getCookies()){
+                    if ("token".equals(cookie.getName())) {
+                        token = cookie.getValue();
+                    }
+                }
+            }
+
+            if (token != null && jwtService.validarToken(token)){
+                String email = jwtService.extrairEmail().orElse(null);
+                Usuario usuario = usuarioRepository.findByEmail(email).orElse(null);
+
+                if (usuario != null) {
+                    UsernamePasswordAuthentication authentication = new UsernamePasswordAuthenticationToken(
+                        usuario, null, null
+                    );
+
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+            
+            filterChain.doFilter(request,response);
+    }
 }
